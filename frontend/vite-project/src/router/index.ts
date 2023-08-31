24f380881c9d4c0350458d8dd47cc5bcfc4a0456
kaki <kaki@aa.com>
2023-08-31 17:44:28 +0900

    aa

--- frontend/vite-project/src/router/index.ts
+++ frontend/vite-project/src/router/index.ts
@@ -1,5 +1,5 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
@@ -18,8 +18,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  // history: createWebHistory("/"),
-  history: createWebHashHistory(),
+  history: createWebHistory(),
+  // history: createWebHashHistory(),
   routes,
 
 
