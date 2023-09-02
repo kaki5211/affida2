590e39fc284e4a9e80b949129afacdd18ad4080f
kaki <kaki@aa.com>
2023-09-02 16:01:58 +0900

    aa

--- frontend/vite-project/src/views/About.vue
+++ frontend/vite-project/src/views/About.vue
@@ -35,7 +35,7 @@ const VIDEOS = computed(() => { return store.getters.GET_VIDEOS; });
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
     <p>ABOUT！</p>
         ■■■VIDEOS：{{ VIDEOS }}
