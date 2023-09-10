381221a4ad98ed34110ece81512cdc57c2c69386
kaki <kaki@aa.com>
2023-09-10 15:47:56 +0900

    aa

--- backend/static/serializer/rest_framework_api.py
+++
@@ -1,7 +0,0 @@
-# from .views import VideoCreateView, VideoAPIView
-from rest_framework import routers
-from .views import VideoViewSet
-
-router = routers.DefaultRouter()
-router.register('videos', VideoViewSet, basename='video')
-# router.register(r'entries', VideoAPIView)
