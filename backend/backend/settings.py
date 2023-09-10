e7db981ef39927272e3eceaa66d77eb5e69cf96b
kaki <kaki@aa.com>
2023-09-10 14:27:42 +0900

    aa

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,9 @@ import os
 import mimetypes
 
 mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
