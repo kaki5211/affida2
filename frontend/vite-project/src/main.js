2ba22ddee0e93d2619ec2316b1d3b76e812cb346
kaki <kaki@aa.com>
2023-09-16 17:23:59 +0900

    aa

--- frontend/vite-project/src/main.js
+++ frontend/vite-project/src/main.js
@@ -7,16 +7,15 @@ import store from './store';
 import { createMetaManager } from 'vue-meta'
 
 // Vuetify
-// import 'vuetify/styles'
-import 'vuetify/dist/vuetify.min.css' // VuetifyのCSSをインポート
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
-// const vuetify = createVuetify({
-//   components,
-//   directives,
-// })
-const vuetify = createVuetify()
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 
 
