76fe3324413cb66f07ac6c8fc6031509dcab24fd
kaki <kaki@aa.com>
2023-10-07 14:43:46 +0900

    aa

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '172.20.10.4', // 希望のアドレスに置き換えてください
+  },
 })
