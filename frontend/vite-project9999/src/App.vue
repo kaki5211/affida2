28bc2334eda43ee9d26f5a447c797ae3891536e4
kaki <kaki@aa.com>
2023-10-07 16:52:18 +0900

    aa

--- frontend/vite-project9999/src/App.vue
+++ frontend/vite-project9999/src/App.vue
@@ -96,6 +96,7 @@ export default defineComponent({
 <template>
 
 <v-app id="#my-scroll-target">
+  <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
   <!-- <Meta /> -->
 
 
