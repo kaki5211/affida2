6fd225f112a0bc022227813a2cb64cb830b7eed8
kaki <kaki@aa.com>
2023-10-07 14:51:21 +0900

    aa

--- frontend/vite-project/src/main.js
+++ frontend/vite-project/src/main.js
@@ -2,4 +2,20 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+
+
+
+
+
+
+
+
+createApp(App)
+// .use(store)
+// .use(router)
+// .use(createMetaManager())
+// .use(metaManager)
+// .use(vuetify)
+.mount('#app')
---
+++ frontend/vite-project/src/meta/index.js
@@ -0,0 +1,6 @@
+// meta.js
+import { createMetaManager } from 'vue-meta';
+
+const metaManager = createMetaManager();
+
+export default metaManager;
